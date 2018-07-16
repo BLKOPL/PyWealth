bf01cb50afc023e75d2d7ee8acbf07d8c7a1f857
Maria <maria.seneca@protonmail.com>
2018-07-16 12:02:08 -0400

    establish foundation and input data

---
+++ config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "i7ecWDE4DWCQfyIPuwPZGcsth"
+consumer_secret = "KBUrivmSihZ6JAWSPX44PjB5M79dpizvp71Q3Gxr387sGFtm2K"
+access_token = "960545558480945152-mFGPJ6EsbtJ6OuKYEHqXfXQGoVgLvn1"
+access_token_secret = "Z43o1szazn3rxcTnoAUIF6RtfeThNsgSDyug7Oiz2ChbV"
